b8663723aca2a27a0ba3b636c33694f3961e0f2a
PolarianDev <polariandev@protonmail.com>
2022-01-21 14:06:38 +0000

    bot base

--- src/main/java/com/github/crafttogether/craftbot/CraftBot.java
+++ src/main/java/com/github/crafttogether/craftbot/CraftBot.java
@@ -1,7 +1,15 @@
 package com.github.crafttogether.craftbot;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
 public class CraftBot {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    private static JDA jda;
+
+    public static void main(String[] args) throws LoginException {
+        jda = JDABuilder.createLight("")
+                .build();
     }
 }
